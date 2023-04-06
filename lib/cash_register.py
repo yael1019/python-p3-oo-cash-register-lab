fe714898156a071d1d7b2c5cd3a8ded4cc9f71ca
Yael Harosh <yaelharosh01@gmail.com>
2023-04-06 13:09:27 -0400

    stopped early

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0, total = 0):
+    self.discount = discount
+    self.total = total
+
+  def add_item(self, title, price, quantity = 1):
+    previous_total = self.total
+    self.total += price * quantity
+
+  def apply_discount(self):
+    self.discount
